bd1422ea6620b38b78e9450d4341adef622ab2a6
Simon31 <saimon3190@yandex.ru>
2022-09-26 16:48:10 +0300

    first commit

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,18 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calculate(int min, int max) {
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= min) {
+                if (i * i <= max)
+                    counter++;
+
+
+
+                return counter;
+            }
+        }
+        return counter;
+    }
+}
